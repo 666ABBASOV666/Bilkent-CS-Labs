4aff513b6ed1abb8b7397743d86b17416586a054
Graff75 <57806876+Graff75@users.noreply.github.com>
2021-10-20 10:49:39 +0300

    Update Lab03_Q2.java

--- lab3/Lab03_Q2.java
+++ lab3/Lab03_Q2.java
@@ -1,70 +1,47 @@
 import java.util.Locale;
 import java.util.Scanner;
 import java.lang.Math;
-public class Lab03_Q3 {
+public class Lab03_Q2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
-        String pin, choice, newBank, newPin;
-        int newPin1;
-        double initial = 1000.0;
-        double withdraw, finalWithdraw;
+        int year, century;
+        String sort;
+
+        System.out.print("Enter the year to be evaluated: ");
+        year = sc.nextInt();
         
-
-        System.out.print("Enter your PIN: ");
-        pin = sc.nextLine();
-
-        if(pin.equals ("1234")){
-
-            System.out.println("1- Withdraw Money \n2- Open a new account \n3- Change PIN");
-            System.out.print("Select an operation: ");
-            choice = sc.nextLine();
-            
-            //option 1 (withdraw)
-            if(choice.equals ("1")){
-                System.out.print("Enter the amount to withdraw: ");
-                withdraw = sc.nextDouble();
-                finalWithdraw = initial - withdraw;
-                System.out.println("Your new balance is " + finalWithdraw + " TRY. Bye!");
-            }
-            //option2 (new bank account)
-            else if(choice.equals ("2")){
-            
-                System.out.println("1- TRY");
-                System.out.println("2- USD");
-                System.out.println("Select currency type:");
-
-                newBank = sc.nextLine();
-                if(newBank.equals ("1")){
-                    System.out.println("Your new TRY account is now opened! Bye!");
-                }
-                else if (newBank.equals ("2")){
-                    System.out.println("Your new USD account is now opened! Bye!");
-                }
-            }    
-                //option 3 (password change)
-                else if (choice.equals ("3")){
-                    System.out.print("Enter your new pin: ");
-                    newPin = sc.next();
-                    newPin1 = Integer.parseInt(newPin);
-                    if (newPin.length() > 4){
-                        System.out.println("Error. Characters limit more than 4!");
-                    } 
-                    else{
-                    char ch1 = newPin.charAt(0);
-                    char ch2 = newPin.charAt(3);
-                    System.out.println("Your new PIN is changed to " + ch1 + "**" + ch2);
-                    }
-                }
-            }    
-         
-        else{
-            System.out.println("Invalid PIN! Bye!");
+        if(year == 0){
+            System.out.println("The year " + year + " is in the 1st century and is a leap year.");
         }
-        sc.close();
+        else{
+        century = (year/100)+1; //century
+        
+        //first, second, third, etc.
+        if( year % 100 == 1){
+            sort = ("st");
+        }
+        else if( year % 100 == 2){
+            sort = ("nd");
+        }
+        else if( year % 100 == 3){
+            sort = ("rd");
+        }
+        else{
+            sort = ("th");
+        }
+        //leap or not a leap
+        if( year % 4 == 0 && year % 100 != 0){
+            System.out.println("The year " + year + " is in the " + century + sort + " century and is a leap year.");
+        }
+        else if(year % 400 == 0){
+            System.out.println("The year " + year + " is in the " + century + sort + " century and is a leap year.");
+        }
+        else{
+            System.out.println("The year " + year + " is in the " + century + sort + " century and is not a leap year.");
+        }
+    }
+    sc.close();
     }
 }
-    
-
-//graff75
